62930bc7fc70edd5c73939d7c6adcb79f74fadd8
BYEONGRYEOL <lyulbyung@gmail.com>
2023-12-22 13:18:47 +0900

    init commit

---
+++ src/main/java/com/hanghae/lemonairlearntest/LemonairLearntestApplication.java
@@ -0,0 +1,13 @@
+package com.hanghae.lemonairlearntest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LemonairLearntestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LemonairLearntestApplication.class, args);
+	}
+
+}
